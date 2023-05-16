11bb908ce2cda1bc41526f45b6f3d6db73622c63
jbeans0312 <jbeans@udel.edu>
2023-05-16 15:04:32 -0400

    finished loading song data

--- fetch_smog.py
+++ fetch_smog.py
@@ -1,6 +1,9 @@
 import spotipy
-from dotenv import load_dotenv
 import os
+import json
+import pandas as pd
+from pathlib import Path
+from dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyClientCredentials
 
 load_dotenv()
@@ -19,19 +22,20 @@ smog_URI = smog_link.split('/')[-1].split('?')[0]
 # get all tracks on playlist (boo spotify api query limits)
 def get_all_tracks(uri):
     tracks_response = sp.playlist_items(uri)
-    t = [x["track"]["uri"] for x in tracks_response["items"]]
+    t = sp.audio_features(x["track"]["uri"] for x in tracks_response["items"])
     while tracks_response["next"]:
         tracks_response = sp.next(tracks_response)
-        t.extend([x["track"]["uri"] for x in tracks_response["items"]])
+        t.extend(sp.audio_features(x["track"]["uri"] for x in tracks_response["items"]))
     return t
 
 
 tracks = get_all_tracks(smog_URI)
 playlist_size = len(tracks)
 
-features = []
+p = Path(r'./smog_data/tracks_out.json')
+with p.open('r', encoding='utf-8') as f:
+    data = json.loads(f.read())
 
-for track in tracks:
-    features.extend(sp.audio_features(tracks[0]))
-
-print(features)
+df = pd.json_normalize(data)
+df = df.dropna()
+df.to_csv('./smog_data/smog.csv', index=False, encoding='utf8')
